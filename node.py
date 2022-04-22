28119c2a97920a0a5c839e22f84dca9ed23dee80
ryker-dev <pizzuti.dev@gmail.com>
2022-04-22 12:39:27 +0300

    Start

--- node.py
+++ node.py
@@ -1,29 +1,36 @@
+from mimetypes import init
 from socketserver import ThreadingMixIn
 from xmlrpc.server import SimpleXMLRPCServer
-import wikipediaapi
+import wikipedia
 
 IP = ("localhost", 3000)
-wiki = wikipediaapi.Wikipedia('en')
+
+class page:
+
+    def __init__(self, title, parent):
+        self.title = title
+        self.parent = parent
+
 
 class SimpleThreadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
     pass
 
 server = SimpleThreadedXMLRPCServer(IP)
 
-class function_class:
+class wikipedia_search:
 
-    def search(self, dest, *args):
-        last = args[0] or ""
-        res = None
-        links = wiki.page(dest).links
+    def search(self, name) -> list:
+        res = wikipedia.search(name)
+        pages = []
 
-        for title in sorted(links.keys()):
-            if (title == dest):
-                return title
-        return links.items()
+        for title in res:
+            if (title == name):
+                continue
+            pages.append(page(title, name))
+        return pages
 
 #### Register functions
-server.register_instance(function_class())
+server.register_instance(wikipedia_search())
 ####
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -3,6 +3,26 @@ from xmlrpc.client import Server, ServerProxy
 ADDRESS = "http://localhost:3000"
 proxy = ServerProxy(ADDRESS, allow_none=True)
 
+class page:
+    
+    def __init__(self, title, parent):
+        self.title = title
+        self.parent = parent
+
+def shortest_path(start, dest):
+        if (start == dest):
+                return start
+
+        depth = proxy.search(start);
+        nextDepth = []
+
+        while True:
+                for p in depth:
+                        print(f'Searching {p}...')
+                        nextDepth.extend([proxy.search(p["title"])])
+        
+                depth = nextDepth
+                break;
+
 if __name__ == '__main__':
-        results = proxy.search("Germany")
-        print(results)
+        shortest_path("Germany", "East Germany")
